b40d74e26bb03a276c86b232cdf72685916952d3
Yakub <ganiyat.yakub@thepalladiumgroup.com>
2023-11-14 09:49:59 +0100

    added lmp to the PMTCT form

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-// export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-// export const url = '/api/v1/'
+export const token = new URLSearchParams(window.location.search).get("jwt");
+export const url = "/api/v1/";
 // export const url = "http://localhost:8383/api/v1/";
 // export const token =
 //   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk5OTMxNDMzfQ.P8btJYpj2dZyC2FQ7hZBoLCFV82IMEiZWF7kV6VuHDZ_A_cccCnxRJoRdoiXlON9V2P-F_Ujg008w0cM8WORvQ";
