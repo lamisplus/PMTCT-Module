ae7c9f38a89359aeb2d519e9321fb2cf790d8a7d
Tech teni <yakubganiyat96@gmail.com>
2023-12-13 14:46:27 +0100

    commented the local token

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-// export const token = new URLSearchParams(window.location.search).get("jwt");
-// export const url = "/api/v1/";
-export const url = "http://localhost:8383/api/v1/";
-export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzAyNDg5OTgyfQ.LLwX1fgN4NXd-UHa-kW1uW-oj6BU6JugVl_atAFXrWXs2-Mr4Ric07Zn9MteSkP6Oq7PX7SW5JAFR2rQ6qudsQ";
+export const token = new URLSearchParams(window.location.search).get("jwt");
+export const url = "/api/v1/";
+// export const url = "http://localhost:8383/api/v1/";
+// export const token =
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzAyNDg5OTgyfQ.LLwX1fgN4NXd-UHa-kW1uW-oj6BU6JugVl_atAFXrWXs2-Mr4Ric07Zn9MteSkP6Oq7PX7SW5JAFR2rQ6qudsQ";
