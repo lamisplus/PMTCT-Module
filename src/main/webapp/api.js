2d6c1a0a75680910507e5e5083332885759c7a8b
Tech teni <yakubganiyat96@gmail.com>
2024-02-15 12:43:59 +0100

    sending input validation changes

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-export const token = new URLSearchParams(window.location.search).get("jwt");
-export const url = "/api/v1/";
-// export const url = "http://localhost:8383/api/v1/";
-// export const token =
-//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA3ODQwOTI4fQ.Roznpgj0IdcaTgePfo1ypM3haELoqsQykfS4m26VYh5xZVtA-CUTuDEJoNqPb1vd4qsQPjjIIYh5UIdjnAOt4w";
+// export const token = new URLSearchParams(window.location.search).get("jwt");
+// export const url = "/api/v1/";
+export const url = "http://localhost:8383/api/v1/";
+export const token =
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA4MDE0OTY1fQ.QZe7TGWZ3vBuoFee-P445kbsEgACx_75q5tjZUH5HuP37h9CXrpJlm32aqebQzvayg3mC8aF0GhdQEIdgLOhQg";
