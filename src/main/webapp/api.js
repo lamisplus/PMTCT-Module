7da58832405e3a7747a632591cc30275571e0ffb
Tech teni <yakubganiyat96@gmail.com>
2024-02-26 10:50:09 +0100

    pushing latest code

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-export const token = new URLSearchParams(window.location.search).get("jwt");
-export const url = "/api/v1/";
-// export const url = "http://localhost:8383/api/v1/";
-// export const token =
-//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA4MDE0OTY1fQ.QZe7TGWZ3vBuoFee-P445kbsEgACx_75q5tjZUH5HuP37h9CXrpJlm32aqebQzvayg3mC8aF0GhdQEIdgLOhQg";
+// export const token = new URLSearchParams(window.location.search).get("jwt");
+// export const url = "/api/v1/";
+export const url = "http://localhost:8383/api/v1/";
+export const token =
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA4NDYzNjAzfQ.WYZgRt4tporDd0KkSl4nP1sipLOu27zspQjhkqeY44L4L41jLVSxn3i218f-nE73gs19Xse61LJ5AmRP6N02Pg";
