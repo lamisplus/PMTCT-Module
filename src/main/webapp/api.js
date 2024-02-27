cbc934c330bcf5105ff4da8622e99752a593fb08
Tech teni <yakubganiyat96@gmail.com>
2024-02-27 15:57:45 +0100

    testing the token

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-// export const token = new URLSearchParams(window.location.search).get("jwt");
-// export const url = "/api/v1/";
-export const url = "http://localhost:8383/api/v1/";
-export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MDUwOTU0fQ.LUAEqeu9SpL40cjL06WiBMCLZCJaebHV40HaCIF029WFEZ--Ye2a0vS25zI3MxtPHwwMkNdzn0FwuRIXVqWCDQ";
+export const token = new URLSearchParams(window.location.search).get("jwt");
+export const url = "/api/v1/";
+// export const url = "http://localhost:8383/api/v1/";
+// export const token =
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MDY3NTYxfQ.8uXN4VSU0dCTtwvJFhfnrS0AbioepUW2JKwRsaU6SGq9TQyopGxphVgz55HNQFk7i0MJna0SPn50uLYuzjEHmQ";
