4d7168f928768217ba8b4570c20b9bf81b82122a
Israel Aderoju <yinkyade@gmail.com>
2024-02-28 18:00:42 +0100

    pushing updated IIT tracking form notitifcation code

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -2,4 +2,4 @@ export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
 // export const url = "http://localhost:8585/api/v1/";
 // export const token =
-//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MDU0OTU1fQ.QYWChwwKyqKGgmnDTGkITjVQpU8T4q94GlJg8EIkGa3XiI0RuFrSyaTLBtkG-D3dVNn47hCZLKRwaTsLZit5Mg";
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MTYxMTMwfQ.g21L-B_k0y1gLU1wL23B-AlExrZDPLDLlmDCX08xqugAENvL2KyyVFZKtvfz8IHhicJWzo1ZSQ0VAINvlXNCWA";
