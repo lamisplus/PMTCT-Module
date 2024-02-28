b7e2a469f7e6c4031c6756acb62a7fe226c65b48
Tech teni <yakubganiyat96@gmail.com>
2024-02-28 17:32:15 +0100

    token

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -2,4 +2,4 @@ export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
 // export const url = "http://localhost:8383/api/v1/";
 // export const token =
-//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MTM3MDU0fQ.D6c1A3AIpXuqVn4phTy7ipVXhS4Ds7Ku61jT-KQsQUQ_xJtX-UgyhGjcDTXt39ovcvj_y1t0f-rvK4d3GE_vGg";
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MTY1OTMyfQ.8aqovaSWp9n9LF1FniO9WhVHRU0K7JbfZ3Lbnblbp470sMplrSILu82s5ddjEPvfsuDJkLj6MX9caV3YvV8s3w";
