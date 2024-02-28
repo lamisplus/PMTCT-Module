007364809e5402aa5496a99734fad15f86d7a050
Tech teni <yakubganiyat96@gmail.com>
2024-02-28 12:28:32 +0100

    Added mother ART section to the pmtct reg form

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-export const token = new URLSearchParams(window.location.search).get("jwt");
-export const url = "/api/v1/";
-// export const url = "http://localhost:8383/api/v1/";
-// export const token =
-//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MDY3NTYxfQ.8uXN4VSU0dCTtwvJFhfnrS0AbioepUW2JKwRsaU6SGq9TQyopGxphVgz55HNQFk7i0MJna0SPn50uLYuzjEHmQ";
+// export const token = new URLSearchParams(window.location.search).get("jwt");
+// export const url = "/api/v1/";
+export const url = "http://localhost:8383/api/v1/";
+export const token =
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MTM3MDU0fQ.D6c1A3AIpXuqVn4phTy7ipVXhS4Ds7Ku61jT-KQsQUQ_xJtX-UgyhGjcDTXt39ovcvj_y1t0f-rvK4d3GE_vGg";
