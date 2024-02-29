608456a154cc4fd7a7145c25fd3579b1f17b79bb
Israel Aderoju <yinkyade@gmail.com>
2024-02-29 09:35:11 +0100

    Code Modification - updated merge conflict across board

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -2,5 +2,5 @@ export const url = process.env.NODE_ENV === "development"
   ? "http://localhost:8585/api/v1/"
   : "/api/v1/";
 export const token = process.env.NODE_ENV === "development"
-  ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MTgxODEzfQ.oo7fQc_7QWNVfjwN_9GXY0ficCHkX2G6ksioH5yfSBBtKdyt2YNIHctCjwyF9S9jNCVjUgz22SmV-w6InhuG1w"
+  ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MjIyOTY5fQ.PZLI2S4ukLhrPv2Fh9SUDmtGcBim2XazZ2I6roi7CkVx9frw3ZzLdMPpkaNJ0JgPlLoUFQr_13ZbGMCsW8KCAw"
   : new URLSearchParams(window.location.search).get("jwt");
