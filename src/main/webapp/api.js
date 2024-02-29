ecef7c5b9d5a66fc5b3efafc543caab8e53eb503
Tech teni <yakubganiyat96@gmail.com>
2024-02-29 09:22:15 +0100

    added es

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-// export const token = new URLSearchParams(window.location.search).get("jwt");
-// export const url = "/api/v1/";
-export const url = "http://localhost:8383/api/v1/";
-export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MTY3Nzk5fQ.HvJgnl7DkKDEBw5TiHWfqq1p2DoNWoQddTbG7wjJdEpw01NMHym_gh_WUpmD7GRdfF-s6CjJ6Asw3lv5q4EfZw";
+export const token = new URLSearchParams(window.location.search).get("jwt");
+export const url = "/api/v1/";
+// export const url = "http://localhost:8383/api/v1/";
+// export const token =
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MTM3MDU0fQ.D6c1A3AIpXuqVn4phTy7ipVXhS4Ds7Ku61jT-KQsQUQ_xJtX-UgyhGjcDTXt39ovcvj_y1t0f-rvK4d3GE_vGg";
