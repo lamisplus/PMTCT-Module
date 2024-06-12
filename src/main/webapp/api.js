0ab8e5ca13a384762d8c84233973a62b7b0e317c
Israel Aderoju <yinkyade@gmail.com>
2024-06-12 12:53:32 +0100

    refactor - relaxing of hiv status text field on EnrollPatient.js, LabourDelivery.js, PmtctEnrollment.js, RegisterPatient.js,UpdatePatientEnrollment.js and inclusion of Time of Diagnosis on ANCEnrollment.js, update as been made across backend on PMTCTEnrollment service, entity and dto.

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
-export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-export const url = '/api/v1/'
-// export const url =  'http://localhost:9090/api/v1/';
-// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjgyMzYxMDM2fQ.csauKx1U0y9o7hefyJREzMXt3DkB_d8meBAex8dNcyV3R6WdzIGjGWVY8qPHj1AJ1bCL5lVExkff1S0RANlLOA'
+//export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+//export const url = '/api/v1/'
+export const url =  'http://localhost:8383/api/v1/';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE4MTU0OTU5fQ.KKNM3OcGi2TqlzVCU8vSvGX8_xO5NrWcuLb5Vlcfejt7pGYmWuaXFtg-efdDy7g3CJVyFz7cpvMLY90oPs3vbg'
