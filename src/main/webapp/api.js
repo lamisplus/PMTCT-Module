bfe46f1ea377b01bb35192156aff37d8c8919260
Tech teni <yakubganiyat96@gmail.com>
2024-06-24 11:27:05 +0100

    editted the normal token

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -2,4 +2,4 @@
 //  export const url = "/api/v1/";
 export const url = "http://localhost:8383/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE5MTczNzc5fQ.EfDYLYwLCcW6LJp6gSM_0AT-jpwfxnT56EwZbqA89IohQNaJWHEIpdobVh660yi4ypiB7pltzNMTXOPszyPJaQ";
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE5MjQ5MTY1fQ.w8A6BHYw9wtLT0jAurDlOu_Rr_RKkmHmskhSYMRPQafEcBys3Kth3uxY7QQuet6cTHyIfYdVzd-zsC6zx9RH5w";
