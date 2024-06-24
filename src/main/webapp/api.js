8fba6c9c8c36bfc32536d8e6aaa5984b46bd941c
Tech teni <yakubganiyat96@gmail.com>
2024-06-24 12:12:50 +0100

    switched to fed brnach

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,5 @@
- export const token = new URLSearchParams(window.location.search).get("jwt");
- export const url = "/api/v1/";
-//export const url = "http://localhost:9090/api/v1/";
-//export const token ="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE4Mjk1OTE0fQ.GtYt4QjW7lLD8G6N0zBN7Gf9nZdeaGki9iHR8ewaJLeiPpf5SjmZGUCh1adwFsCTqA8LCki4CB5mCXWJNeBeBw";
+//  export const token = new URLSearchParams(window.location.search).get("jwt");
+//  export const url = "/api/v1/";
+export const url = "http://localhost:8383/api/v1/";
+export const token =
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE5MjUzNTI5fQ.enDVd-lo792JZzVU7w7z5OdNdv0J41ggiKo0esHbL-GzwcjwULvmsVroOCgUladE2qxn2ePp_uoQq3e0Uwaxsg";
